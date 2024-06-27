d23096922af2f267706c9297b37b889cd3a15430
pedrorosa-dev <devpedrorosa.c@gmail.com>
2024-06-27 09:53:21 -0300

    add relative number only normal mode

--- .config/lvim/lua/user/keybinds.lua
+++ .config/lvim/lua/user/keybinds.lua
@@ -1,7 +1,6 @@
 lvim.keys.normal_mode["<Leader>h"] = ":set wrap<CR>"
 -- lvim.keys.normal_mode["<C-s>"] = ":w<CR>"
 lvim.keys.insert_mode["kj"] = "<Esc>"
-vim.cmd([[set relativenumber]])
 vim.api.nvim_set_keymap("x", "<C-k>", ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("x", "<C-j>", ":move '>+1<CR>gv-gv", { noremap = true, silent = true }) -- lvim.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 vim.api.nvim_set_keymap("n", "<S-A-x>", ":BufDel *<CR>", { noremap = true, silent = true })
@@ -20,5 +19,18 @@ lvim.builtin.bufferline.animation = true
 lvim.builtin.bufferline.icons = true
 lvim.builtin.bufferline.icon_custom_colors = true
 
-
 vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', { noremap = true })
+
+
+-- vim.cmd([[set relativenumber]])
+
+-- Ativar numeração relativa no modo normal
+vim.cmd([[
+  augroup NumberToggle
+    autocmd!
+    autocmd InsertEnter * set norelativenumber
+    autocmd InsertLeave * set relativenumber
+    autocmd CmdlineEnter * set norelativenumber
+    autocmd CmdlineLeave * set relativenumber
+  augroup END
+]])
--- .zshrc
+++ .zshrc
@@ -126,3 +126,5 @@ export PATH="$(pyenv root)/shims:$PATH"
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+
