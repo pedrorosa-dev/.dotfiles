ccff2e5d126a730dd4426c4f31666e0f4da290d7
pedrorosa-dev <devpedrorosa.c@gmail.com>
2024-06-29 17:46:38 -0300

    move explorer to left side nvimtree

--- .local/share/lunarvim/lvim/lua/lvim/core/nvimtree.lua
+++ .local/share/lunarvim/lvim/lua/lvim/core/nvimtree.lua
@@ -30,7 +30,7 @@ function M.config()
         width = 34,
         cursorline = true,
         debounce_delay = 15,
-        side = "right",
+        side = "left",
         preserve_window_proportions = false,
         number = false,
         relativenumber = false,
