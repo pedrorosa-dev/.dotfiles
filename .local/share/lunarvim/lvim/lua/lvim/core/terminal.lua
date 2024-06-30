62afd494ce7def9cdfdcb27770d99bacaf9e2e5c
pedrorosa-dev <devpedrorosa.c@gmail.com>
2024-06-29 21:03:51 -0300

    settings terminal

--- .local/share/lunarvim/lvim/lua/lvim/core/terminal.lua
+++ .local/share/lunarvim/lvim/lua/lvim/core/terminal.lua
@@ -41,8 +41,8 @@ M.config = function()
     -- lvim.builtin.terminal.execs[#lvim.builtin.terminal.execs+1] = {"gdb", "tg", "GNU Debugger"}
     -- TODO: pls add mappings in which key and refactor this
     execs = {
-      { nil, "<C-x>", "Horizontal Terminal", "horizontal", 0.3 },
-      { nil, "<S-x>", "Vertical Terminal",   "vertical",   0.4 },
+      { nil, "<S-x>", "Horizontal Terminal", "horizontal", 0.3 },
+      { nil, "<C-x>", "Vertical Terminal",   "vertical",   0.4 },
       { nil, "<S-z>", "Float Terminal",      "float",      nil },
     },
   }
@@ -172,4 +172,3 @@ M.lazygit_toggle = function()
 end
 
 return M
-
